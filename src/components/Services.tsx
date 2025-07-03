d880e178d60db14c24dfb2ea84c5a5f23c3ea162
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2025-07-03 00:34:37 -0400

    seervices error fixed by comment

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { Social, Events, Commercial, Private } from '@Assets/images/image.barrel';
+// import { Social, Events, Commercial, Private } from '@Assets/images/image.barrel';
 
 const services = [
   {
