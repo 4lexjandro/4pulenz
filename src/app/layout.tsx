92a39dd56867f3aa2d48f6003ab7a7691877ae22
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2025-09-28 14:47:33 -0400

    changed ig name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
                   className='transition duration-200 hover:brightness-125 hover:scale-110 hover:drop-shadow-md [filter:invert(27%) sepia(85%) saturate(400%) hue-rotate(280deg) brightness(100%) contrast(105%)]'
                 />
               </Link>
-              <Link href='https://www.instagram.com/4lexjandro_/' target='_blank'>
+              <Link href='https://www.instagram.com/4pulenz/' target='_blank'>
                 <Image
                   alt='Instagram'
                   src={Instagram}
